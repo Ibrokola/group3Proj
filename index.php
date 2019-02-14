893acbdc099ceb32cd52da637bc4f48d3adad365
ibro <kabaltech@gmail.com>
2019-02-13 20:34:09 -0700

    update: added a little animation

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/8.0.0/sanitize.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css">
         <link rel="stylesheet" href="css/style.css">
     </head>
 
@@ -64,11 +65,11 @@
             <!-- End Landing Carousel -->
 
             <!-- Start landing Jumbotron -->
-            <div class="jumbotron jumbotron-fluid jumbotron-landing">
+            <div class="jumbotron jumbotron-fluid jumbotron-landing wow zoomIn" data-wow-delay="100ms">
                 <div class="container">
                     <?php include('php/includes/time.php'); ?>
-                    <h1 class="display-4 index-greeting">Ready to see world?</h1>
-                    <p class="lead">Let's do it together...</p>
+                    <h1 class="display-4 index-greeting wow fadeInDown" data-wow-delay="450ms">Ready to see world?</h1>
+                    <p class="lead wow bounceInUp" data-wow-delay="500ms">Let's do it together...</p>
                 </div>
             </div>
 
@@ -97,7 +98,7 @@
                 <!-- <hr> -->
                 <br>
                     <div class="card-deck">
-                        <div class="card package">
+                        <div class="card package wow slideInLeft" data-wow-delay="500ms">
                             <img src="img/carib-new.jpeg" class="card-img-top" alt="caribbean">
                             <div class="card-body">
                                 <h4 class="card-title h4-responsive"> <strong> Caribbean New Year</strong></h4>
@@ -109,7 +110,7 @@
                                 </footer>
                             </blockquote>
                         </div>
-                        <div class="card package">
+                        <div class="card package wow slideInUp" data-wow-delay="0.1s">
                             <img src="img/Polyn.jpeg" class="card-img-top" alt="polynesian">
                             <div class="card-body">
                                 <h4 class="card-title h4-responsive"> <strong> Polynesian Paradise</strong></h4>
@@ -121,7 +122,7 @@
                                 </footer>
                             </blockquote>
                         </div>
-                        <div class="card package">
+                        <div class="card package wow slideInRight" data-wow-delay="0.2s">
                             <img src="img/Asian.jpeg" class="card-img-top" alt="asian">
                             <div class="card-body">
                                 <h4 class="card-title h4-responsive"> <strong>Asian Expedition </strong></h4>
@@ -152,7 +153,12 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
         <script src="js/main.js"></script>
+
+        <script>
+            new WOW().init();
+        </script>
         <!-- End JavaScript -->
 
     </body>
