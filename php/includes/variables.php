e9bd80e5c88e94c038a1af9ebbd517e4fba0c363
ibro <kabaltech@gmail.com>
2019-02-13 21:45:24 -0700

    update: added every members name

--- php/includes/variables.php
+++ php/includes/variables.php
@@ -7,8 +7,8 @@
 ****************************************/
 
     $assoc_links = array(
-        "http://www.calgary.ca/SitePages/cocis/default.aspx" => "See calgary",
-        "http://www.calgary.ca/SitePages/cocis/default.aspx" => "See calgary",
+        "http://www.calgary.ca/SitePages/cocis/default.aspx" => "See Calgary",
+        "http://www.calgary.ca/SitePages/cocis/default.aspx" => "See Calgary",
         "https://www.edmonton.ca/" => "See Edmonton",
         "https://vancouver.ca/" => "See Vancouver",
         "https://www.saskatoon.ca/" => "See Saskatoon",
